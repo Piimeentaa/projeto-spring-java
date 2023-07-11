089c31bf87fd91e6f7dc53ff44f3c9730be7639b
Piimeentaa <dora13.isa@gmail.com>
2023-07-10 21:25:07 -0300

    Project created

---
+++ src/test/java/com/pimentadakota/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimentadakota.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
